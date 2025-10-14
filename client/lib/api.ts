2cc6e06ba1a68a7d4c1943384c54c07ed230f4dd
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2025-10-14 16:10:55 +0600

    Update api.ts

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -17,7 +17,7 @@ export const api = axios.create({
 api.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
+    if (error.response?.status === 401 || error.response?.status === 403) {
       // Токен истек или недействителен
       localStorage.removeItem('token');
       delete api.defaults.headers.common['Authorization'];
