758129ae6321369916e22db1582013fd8025d565
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2025-11-24 18:42:57 +0600

    Update TravelersGuild.java

--- server/mcraft/plugins/TravelersGuild/src/main/java/com/travelersguild/TravelersGuild.java
+++ server/mcraft/plugins/TravelersGuild/src/main/java/com/travelersguild/TravelersGuild.java
@@ -6,6 +6,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Monster;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.event.player.PlayerJoinEvent;
